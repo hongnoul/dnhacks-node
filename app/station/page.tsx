b9cb4635d4d2ba69b859a35b1cf0afc0f7308ad2
Justin Hong <namyang@mit.edu>
2026-09-06 02:48:34 -0400

    Remove nav bar from dashboard

--- app/station/page.tsx
+++ app/station/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { OperationsHeader } from "../lib/DesignSystem";
 import { JoinCard } from "../lib/JoinCard.tsx";
 import { DroneStage } from "../lib/DroneStage.tsx";
 import { AdminDashboard } from "../lib/AdminDashboard.tsx";
 
 export default function StationPage() {
   return <>
-    <OperationsHeader />
     <main className="unified-console">
       <AdminDashboard onboarding={<>
         <div className="join-card"><JoinCard compact /></div>
